eb0cbd6a302fd4ee3fddec28467c51aebefcf10b
August Duelund <August@dcit.dk>
2021-03-08 22:03:02 +0100

    Close server in progress

--- src/main/java/server/ChatServer.java
+++ src/main/java/server/ChatServer.java
@@ -1,11 +1,41 @@
 package server;
 
 
+import com.sun.xml.internal.bind.v2.TODO;
+
+import java.io.PrintWriter;
 import java.net.InetAddress;
 import java.net.ServerSocket;
 import java.net.UnknownHostException;
 
 public class ChatServer {
+    private boolean commandInput(String msg, PrintWriter pw) {
+        String[] parts = msg.split("#");
+        if (parts.length == 1) {
+            if (parts[0].equals("CLOSE")) {
+                pw.println("CLOSE#");
+            }
+            else {
+                pw.println("Unknown request - must obey protocol");
+            }
+            return false;
+        } else if (parts.length == 2) {
+            String token = parts[0];
+            String argument = parts[1];  //Værdi efter #-tegnet
+            switch (token) {
+                case "CONNECT":
+                    break;
+                case "SEND":
+                    break;
+                default:
+                    pw.println("Invalid input - closing connection...");
+                    return false;
+            }
+        }
+        return true;
+    }
+
+    //Lav client handler med socket og kald socket.close()
 
 
     //Call server with arguments like this: 0.0.0.0 8088 logfile.log
